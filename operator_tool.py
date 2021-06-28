18e90544ecd4e025fb4022a5973276886962ac47
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2021-06-28 17:10:15 -0400

    updated to search for rip .xml output file + copy into orders dir instead of looking up .prn files

--- operator_tool.py
+++ operator_tool.py
@@ -8,24 +8,17 @@ import os
 import traceback
 import datetime
 import time
+import shutil
 
-# Change "Y:" to the windows drive that has the network share mounted for the printer's "jobs" directory
-JOBS_DIRECTORY = 'Y:\\'
-
-# Use this folder if we are on the printer
-if os.path.exists("/home/aeoon/AeoonPrint/jobs"):
-  JOBS_DIRECTORY = "/home/aeoon/AeoonPrint/jobs"
+# Change "Y:" to the windows folder that contains already-ripped .xml files.
+RIPPED_XML_OUT_DIRECTORY = r'C:\Users\17044\Desktop\test automation\done'
 
 # For testing
-if 'JOBS_DIRECTORY' in os.environ:
-  JOBS_DIRECTORY = os.environ['JOBS_DIRECTORY']
+if 'RIPPED_XML_OUT_DIRECTORY' in os.environ:
+  RIPPED_XML_OUT_DIRECTORY = os.environ['RIPPED_XML_OUT_DIRECTORY']
 
 # Change "X:" to the windows drive that has the network share mounted for the printer's "orders" directory
 PRINT_ORDERS_HOTFOLDER = 'X:\\'
-
-# Use this folder if we are on the printer
-if os.path.exists("/home/aeoon/AeoonPrint/order_jobs"):
-  PRINT_ORDERS_HOTFOLDER = "/home/aeoon/AeoonPrint/order_jobs"
 
 # For testing
 if 'PRINT_ORDERS_HOTFOLDER' in os.environ:
@@ -41,16 +34,16 @@ def get_user_input(prompt='> '):
   else:
     return input(prompt)
 
-def search_for_prn(upc):
+def search_for_rip_xml(upc):
   """
-  Searches through JOBS_DIRECTORY for a file ending in .prn that contains the value of upc,
+  Searches through RIPPED_XML_OUT_DIRECTORY for a file ending in .xml that contains the value of upc,
   and returns the first match
   """
-  for dirpath, dirnames, filenames in os.walk(JOBS_DIRECTORY):
+  for dirpath, dirnames, filenames in os.walk(RIPPED_XML_OUT_DIRECTORY):
     # Do not search in already-printing items
     
     for file in filenames:
-      if upc in file.lower() and file.lower().endswith('.prn'):
+      if upc in file.lower() and file.lower().endswith('.xml'):
         # We found it!
         return os.path.join(dirpath, file)
 
@@ -71,71 +64,19 @@ def main(args=sys.argv):
       if len(upc) < 3:
         continue
 
-      prn_file = search_for_prn(upc)
+      xml_file = search_for_rip_xml(upc)
 
-      if not prn_file:
-        print('[error] Could not find a matching PRN file, has this been ripped yet?')
+      if not xml_file:
+        print('[error] Could not find a matching rip .xml output file, has this been ripped yet?')
         get_user_input('Press Enter to continue...')
         continue
 
-      prn_file_basename = os.path.basename(prn_file)
-      job_name = prn_file_basename.replace('.prn', '')
-      job_xml_file = os.path.join(PRINT_ORDERS_HOTFOLDER, job_name+'.xml')
+      xml_file_basename = os.path.basename(xml_file)
+      job_xml_file = os.path.join(PRINT_ORDERS_HOTFOLDER, xml_file_basename)
 
-      print("Inserting job {} from {} into {}".format(job_name, prn_file, job_xml_file))
+      print("Inserting job {} from {} into {}".format(xml_file_basename, xml_file, job_xml_file))
 
-      with open(job_xml_file, 'w') as fd:
-        fd.write('''
-<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-
-<Order>
-  <Id>{job_name}</Id>
-  <Images>
-    <Image>
-      <Id>{job_name}-i0</Id>
-      <SourceImage>/home/aeoon/AeoonPrint/jobs/{job_name}.png</SourceImage>
-      <RipedImage>{prn_file_basename}</RipedImage>
-      <RipProfile>shirt-white</RipProfile>
-      <ColorPasses>3</ColorPasses>
-      <Size>
-        <Width>100</Width>
-        <Height>240</Height>
-      </Size>
-      <Rotation>0</Rotation>
-      <RipStatus>
-        <StatusCode>1</StatusCode>
-        <Message>Ok</Message>
-      </RipStatus>
-    </Image>
-  </Images>
-  <Products>
-    <Product>
-      <Id>{job_name}-p0</Id>
-      <DesiredCount>1</DesiredCount>
-      <Material>AAA of the Loom white, L</Material>
-      <Prints>
-        <Print>
-          <Id>{job_name}-p0p1</Id>
-          <ImageId>{job_name}-i0</ImageId>
-          <PrintArea>front</PrintArea>
-          <Position>
-            <X>0</X>
-            <Y>0</Y>
-          </Position>
-        </Print>
-      </Prints>
-    </Product>
-  </Products>
-</Order>
-'''.format(
-  job_name=job_name,
-  prn_file_basename=prn_file_basename,
-  # TODO future parameters I see needing:
-  # print resolution (width x height)
-  # print location (x,y)
-  # RipProfile and ColorPasses
-  # Possibly Material, though this looks like it's purely for humans to read and would not be processed by the printer
-).strip())
+      shutil.copy(xml_file, job_xml_file)
 
       print('Wrote job to {}, polling for 10s to see if Aeoon accepts it...'.format(job_xml_file))
       aeoon_accepted_xml = False
